1b45cc47e0744bec99f3960621475994a153f113
Melina Djedaimi <melina.djedaimi@efrei.net>
2025-12-18 02:09:29 +0100

    Création du contrôleur Home et de la vue

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\CategoryRepository;
+use App\Repository\ProductRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class HomeController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    public function index(CategoryRepository $categoryRepository, ProductRepository $productRepository): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'categories' => $categoryRepository->findAll(),
+            'products' => $productRepository->findAll(),
+        ]);
+    }
+}
